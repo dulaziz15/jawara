3dbf5902813ab2d5e63c15eea0c38a0e0db4bf59
ABDUL AZIZ <107160974+dulaziz15@users.noreply.github.com>
2025-10-21 19:17:20 +0700

    revisi dashboard

--- lib/presentation/pages/warga/tambah_warga.dart
+++ lib/presentation/pages/warga/tambah_warga.dart
@@ -6,7 +6,138 @@ class WargaTambahPage extends StatelessWidget {
   const WargaTambahPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text("Tambah Warga Page", style: TextStyle(fontSize: 22)));
+    return Container(
+      margin: const EdgeInsets.all(24),
+      padding: const EdgeInsets.all(24),
+      width: 550,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const SizedBox(height: 15),
+          const Center(
+            child: Text(
+              'Tambah Warga',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(height: 16),
+          const Center(
+            child: Text(
+              'Form untuk menambahkan data warga',
+              style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),
+            ),
+          ),
+          const SizedBox(height: 24),
+          const Text('NIK', style: TextStyle(fontSize: 14)),
+          const SizedBox(height: 5),
+          const TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Masukkan NIK',
+            ),
+          ),
+          const SizedBox(height: 16),
+          const Text('Nama Lengkap', style: TextStyle(fontSize: 14)),
+          const SizedBox(height: 5),
+          const TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Masukkan Nama Lengkap',
+            ),
+          ),
+          const SizedBox(height: 16),
+          const Text('Alamat', style: TextStyle(fontSize: 14)),
+          const SizedBox(height: 5),
+          const TextField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'Masukkan Alamat',
+            ),
+          ),
+          const SizedBox(height: 16),
+          const Text('Status Kepemilikan', style: TextStyle(fontSize: 14)),
+          const SizedBox(height: 5),
+          DropdownButtonFormField<String>(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(4),
+              ),
+              contentPadding: const EdgeInsets.symmetric(
+                horizontal: 12,
+                vertical: 12,
+              ),
+            ),
+            hint: const Text("-- Pilih Status Kepemilikan --"),
+            items: const [
+              DropdownMenuItem(
+                value: "Milik Sendiri",
+                child: Text("Milik Sendiri"),
+              ),
+              DropdownMenuItem(value: "Sewa", child: Text("Sewa")),
+              DropdownMenuItem(value: "Kontrak", child: Text("Kontrak")),
+            ],
+            onChanged: (String? newValue) {},
+          ),
+          const SizedBox(height: 24),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  context.router.pop();
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 255, 22, 22),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 15,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: const Text(
+                  'Batal',
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ),
+              const SizedBox(width: 16),
+              ElevatedButton(
+                onPressed: () {
+                  // Simpan data warga
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF6C63FF),
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 24,
+                    vertical: 15,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: const Text(
+                  'Simpan',
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
