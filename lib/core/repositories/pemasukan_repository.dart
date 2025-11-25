ce769d51463455d1f10ada3003e9e307bd6b05d4
Fahrizp24 <Fpradian24@gmail.com>
2025-11-25 22:26:08 +0700

    read pengeluaran dan daftar pemasukan

--- lib/core/repositories/pemasukan_repository.dart
+++ lib/core/repositories/pemasukan_repository.dart
@@ -20,16 +20,39 @@ class PemasukanRepository {
         .map((snapshot) {
           return snapshot.docs.map((doc) {
             final data = doc.data() as Map<String, dynamic>;
-            return PemasukanModel.fromMap({...data, 'id': doc.id});
+            return PemasukanModel.fromMap({...data});
           }).toList();
         });
   }
 
+  // U: Update Pemasukan
   Future<void> updatePemasukan(PemasukanModel pemasukan) async =>
       await _pemasukanCollection
           .doc(pemasukan.id.toString())
           .update(pemasukan.toMap());
-          
+
+  // D: Delete Pemasukan
   Future<void> deletePemasukan(String pemasukanId) async =>
       await _pemasukanCollection.doc(pemasukanId).delete();
+
+  // R: Get Pemasukan by int id (Future)
+  Future<PemasukanModel?> getPemasukanByIntId(int id) async {
+    try {
+      final snapshot = await _pemasukanCollection
+          .where('id', isEqualTo: id)
+          .limit(1)
+          .get();
+
+      if (snapshot.docs.isNotEmpty) {
+        final doc = snapshot.docs.first;
+        final data = doc.data() as Map<String, dynamic>;
+
+        // PANGGIL with fromMap (Sekarang pakai 2 parameter)
+        return PemasukanModel.fromMap(data);
+      }
+      return null;
+    } catch (e) {
+      throw Exception('Gagal: $e');
+    }
+  }
 }
